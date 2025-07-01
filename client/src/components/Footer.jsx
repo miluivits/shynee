e92e9486caada577db4b735cfce92a983742f218
miluivits <ivitsmilu@gmail.com>
2025-07-01 17:32:48 +0200

    Added footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+import simplepayLogo from '../images/logos/simplepay.svg';
+import visaLogo from '../images/logos/Visa_Inc._logo.svg';
+import mastercardLogo from '../images/logos/Mastercard.svg';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-main">
+        <div className="footer-section left">
+          <h2 className="footer-logo">Shynee</h2>
+
+          <div className="footer-hours">
+            <ul>
+              <li><span>Hétfő - Péntek:</span> 09:00 - 18:00</li>
+              <li><span>Szombat:</span> 10:00 - 14:00</li>
+              <li><span>Vasárnap:</span> Zárva</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footer-section center">
+          <h4>Menü</h4>
+          <nav>
+            <Link to="/">Főoldal</Link>
+            <Link to="/rolunk">Rólunk</Link>
+            <Link to="/kapcsolat">Kapcsolat</Link>
+          </nav>
+        </div>
+
+        <div className="footer-section right">
+          <h4>Kövess minket</h4>
+          <div className="social-icons">
+            <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer"><FaInstagram /></a>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+          </div>
+        </div>
+
+        <div className="payment-logos">
+          <img src={simplepayLogo} alt="SimplePay" />
+          <img src={visaLogo} alt="Visa" />
+          <img src={mastercardLogo} alt="Mastercard" />
+        </div>
+      </div>
+
+      <p className="copyright">© 2025 Minden jog fenntartva.</p>
+    </footer>
+  );
+}
--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import './Layout.css';
+import Footer from './Footer';
 
 export default function Layout() {
   const { pathname } = useLocation();
-  
+
   const pageClass =
-    pathname === '/rendeles'  ? 'page-rendeles' :
-    pathname === '/rolunk'    ? 'page-rolunk'   :
-    pathname === '/media'     ? 'page-media'    :
-    pathname === '/kapcsolat' ? 'page-kapcsolat':
-    pathname === '/rendeles-kulso' ? 'page-rendeles-kulso':
-    pathname === '/rendeles-belso' ? 'page-rendeles-belso':
-    pathname === '/rendeles-kulso-belso' ? 'page-rendeles-kulso-belso':
-                                 'page-home';
+    pathname === '/rendeles' ? 'page-rendeles' :
+    pathname === '/rolunk' ? 'page-rolunk' :
+    pathname === '/media' ? 'page-media' :
+    pathname === '/kapcsolat' ? 'page-kapcsolat' :
+    pathname === '/rendeles-kulso' ? 'page-rendeles-kulso' :
+    pathname === '/rendeles-belso' ? 'page-rendeles-belso' :
+    pathname === '/rendeles-kulso-belso' ? 'page-rendeles-kulso-belso' :
+    'page-home';
 
   return (
     <div className={`page-container ${pageClass}`}>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
